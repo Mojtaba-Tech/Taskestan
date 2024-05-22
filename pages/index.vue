f62093edae2731f9a38cf83fc8f71a1b699d8ac7
Mojtaba <smo.hayati@gmail.com>
2024-05-22 22:25:50 +0800

    Add boards page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,39 @@
 <template>
 	<div>
-		<h1>Your Boards</h1>
+		<h1 class="text-white bg-stone-800/70 border-l-[10px] border-4 border-stone-800 rounded-large p-4">Your Boards</h1>
 		
-		
+		<section class="flex flex-wrap py-10">
+			<div class="flex justify-center items-center bg-white/90 hover:bg-white transition cursor-pointer rounded-medium w-[calc((100%/3)-16px)] h-56 p-4 border-4 border-slate-600/80 mb-3 mx-2">
+				<div class="text-center text-slate-600/90">
+					<font-awesome :icon="faPlus" size="3x"/>
+					<h3 class="mt-2">Add new board</h3>
+				</div>
+			</div>
+			<div class="bg-white/80 rounded-medium w-[calc((100%/3)-16px)] h-56 p-4 border-t-8 border-2 border-slate-600/80 mb-3 mx-2">
+				<h2 class="mb-3">Udemy projects</h2>
+				<p>This is about my Udemy tasks</p>
+			</div>
+			<div class="bg-white/80 rounded-medium w-[calc((100%/3)-16px)] h-56 p-4 border-t-8 border-2 border-slate-600/80 mb-3 mx-2">
+				<h2 class="mb-3">Udemy projects</h2>
+				<p>This is about my Udemy tasks</p>
+			</div>
+			<div class="bg-white/80 rounded-medium w-[calc((100%/3)-16px)] h-56 p-4 border-t-8 border-2 border-slate-600/80 mb-3 mx-2">
+				<h2 class="mb-3">Udemy projects</h2>
+				<p>This is about my Udemy tasks</p>
+			</div>
+			<div class="bg-white/80 rounded-medium w-[calc((100%/3)-16px)] h-56 p-4 border-t-8 border-2 border-slate-600/80 mb-3 mx-2">
+				<h2 class="mb-3">Udemy projects</h2>
+				<p>This is about my Udemy tasks</p>
+			</div>
+			<div class="bg-white/80 rounded-medium w-[calc((100%/3)-16px)] h-56 p-4 border-t-8 border-2 border-slate-600/80 mb-3 mx-2">
+				<h2 class="mb-3">Udemy projects</h2>
+				<p>This is about my Udemy tasks</p>
+			</div>
+		</section>
 	</div>
 </template>
+
+<script setup>
+import {faPlus} from '@fortawesome/free-solid-svg-icons'
+
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        small: '4px',
+        medium: '8px',
+        large: '12px',
+        huge: '24px'
+      }
+    },
   },
   plugins: [],
 }
