e3c74d9932f70416b3ea13216b95ac9e8f007efa
Mojtaba <smo.hayati@gmail.com>
2024-08-10 23:50:31 +0800

    Task component completed

--- components/editor/Category.vue
+++ components/editor/Category.vue
@@ -25,23 +25,9 @@
 		<section>
 			
 			<!-- Task Item -->
-			<div
-				v-for="i in 5"
-				class="bg-white border border-black/10 rounded-lg p-6 shadow-2 mb-3"
-			>
-				<h3 class="text-13 text-gray-700 font-normal mb-1">
-					Enhance browser section
-				</h3>
-				<span class="text-xs text-gray-300">
-					Oct 29 - Oct 30
-				</span>
-				<div class="flex items-center justify-end pt-16">
-					<PublicTag
-						text="New"
-						:theme="color as 'cyan' | 'orange' | 'red' | 'green'"
-					/>
-				</div>
-			</div>
+			<template	v-for="i in 3">
+				<EditorTask :color="color" />
+			</template>
 			<!-- End Task Item -->
 		
 		</section>
---
+++ components/editor/Task.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="bg-white border border-black/10 rounded-lg p-6 shadow-2 mb-3">
+		<h3 class="text-13 text-gray-700 font-normal mb-1">
+			Enhance browser section
+		</h3>
+		<span class="text-xs text-gray-300">
+			Oct 29 - Oct 30
+		</span>
+		<div class="flex items-center justify-end pt-16">
+			<PublicTag
+				text="New"
+				:theme="color as 'cyan' | 'orange' | 'red' | 'green'"
+			/>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+	color: string
+}>()
+</script>
+
+<style scoped>
+
+</style>
