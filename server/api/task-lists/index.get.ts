0b598ca72d21fbb4dbeebce10e053cf77de85549
Mojtaba <smo.hayati@gmail.com>
2024-10-09 06:38:29 +0800

    Fix get task lists bug

--- server/api/task-lists/index.get.ts
+++ server/api/task-lists/index.get.ts
@@ -2,7 +2,8 @@ import {serverSupabaseClient} from "#supabase/server";
 
 export default defineEventHandler(async (event) => {
   const client = await serverSupabaseClient(event)
-  const {data} = await client.from('taskLists').select()
+  const {boardId} = await getQuery(event)
+  const {data} = await client.from('taskLists').select().eq('board_id', boardId as string)
 
   return data;
 })
